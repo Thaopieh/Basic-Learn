9157902fca4292a52d5bc83f2360696a6bc2c72c
Thao <0trinhthao1@gmial.com>
2024-01-29 23:37:08 +0700

    doi

--- hi.py
+++ hi.py
@@ -1,1 +1,8 @@
-print('Hello')
+import numpy as np
+from sklearn.linear_model import LinearRegression
+X = np.array([[1,1],[1,2], [2,2],[2,3],[3,5]])
+y = np.dot(X, np.array([1,2])) + 3
+regr = LinearRegression(
+   fit_intercept = True, copy_X = True, n_jobs = 2
+).fit(X,y)
+regr.predict(np.array([[3,5]]))
